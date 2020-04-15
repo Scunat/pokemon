b17daabc8053724c5d0ab8380bc61733b75a51ab
Sarita Cunat <cunat.sarita@gmail.com>
2020-04-15 11:07:57 +0200

    list berries

--- js/poke.js
+++ js/poke.js
@@ -1,32 +1,21 @@
 $(() => {
-    $.get("https://pokeapi.co/api/v2/berry/1", function (data) {
-        $("#result").html(" ");
+    $.get("https://pokeapi.co/api/v2/berry/", function (data) {
+        $("#recup").html(" ");
+        
         for (let index in data) {
+            console.log(data);
             if (typeof data[index] === "object") {
-                //let films = data[index].toString().split(",");
-                $("#result").html($("#result").html() + "<br/> -" + index + " : Tableau : ");
                 for (let numF in data[index]) {
 
                     if (typeof data[index][numF] === "object") {
-                        //let films = data[index].toString().split(",");
-                        $("#result").html($("#result").html() + "<br/> -" + numF + " : Tableau : ");
                         for (let numF2 in data[index][numF]) {
-                            $("#result").html($("#result").html() + "<br/> -" + numF2 + " : " + data[index][numF][numF2]);
-
+                            if(numF2 == "name") {
+                            $("#recup").html($("#recup").html() + "<p>" + data[index][numF][numF2] + "</p>");
+                            }
                         }
-                    }
-                    else {
-                        $("#result").html($("#result").html() + "<br/> -" + numF + " : " + data[index][numF]);
                     }
                 }
             }
-            else {
-                $("#result").html($("#result").html() + "<br/> -" + index + " : " + data[index]);
-            }
-
-
         }
-
-        console.log(data)
     });
 });
